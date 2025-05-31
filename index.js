39f0f359964def0bf93c6e090d5c24cd12bf52d0
Judith Camille Rosette <167699879+helicohedron@users.noreply.github.com.>
2025-05-31 19:42:30 +0800

    feat: function that lets user play a single round of rock, paper, scissors

--- index.js
+++ index.js
@@ -15,12 +15,58 @@ function getComputerChoice() {
   } 
 }
 
-// console.log(getComputerChoice());
-
 // function that prompts the user for either rock, paper, or scissors
 function getHumanChoice() {
   let userInput = prompt('Rock, Paper, or Scissors?');
   return(userInput.toLowerCase());
 }
 
-console.log(getHumanChoice());
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+// function for a single round of rock, paper, scissors
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    return "It's a draw.";
+  } 
+
+  if (humanChoice == 'rock') {
+    switch(computerChoice) {
+      case 'paper':
+        return 'Computer wins!';
+        break;
+      case 'scissors':
+        return 'Human wins!';
+        break;
+    }
+  }
+
+    if (humanChoice == 'paper') {
+    switch(computerChoice) {
+      case 'scissors':
+        return 'Computer wins!';
+        break;
+      case 'rock':
+        return 'Human wins!';
+        break;
+    }
+  }
+
+    if (humanChoice == 'scissors') {
+    switch(computerChoice) {
+      case 'rock':
+        return 'Computer wins!';
+        break;
+      case 'paper':
+        return 'Human wins!';
+        break;
+    }
+  }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+console.log(playRound(humanChoice, computerChoice));
+
+
+
